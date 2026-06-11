7424ca1b6dc3141bffda25ee52124e40af4b9c09
masterix_ms <git@sintracon.de>
2026-06-11 17:50:35 +0200

    feat: implement a C preprocessor macro for debug_printing

--- mini_webserv.h
+++ mini_webserv.h
@@ -9,17 +9,32 @@
 #include <errno.h>
 #include <stdio.h>
 
-//types
-typedef struct client_s{
-	int	fd;
-	int	id;
-	char	in[1024];
-	char	out[1024];
-}	client_t;
+// Define the debug print mode as a C preprocessor macro.
+// If DEBUG is defined, the DEBUG_PRINT macro will print the provided message (variadic macro). Otherwise, it will do nothing.
+#ifdef DEBUG
+#define DEBUG_PRINT(...)                    \
+	do                                      \
+	{                                       \
+		printf("DEBUG_INFO: " __VA_ARGS__); \
+	} while (0)
+#else
+#define DEBUG_PRINT(...) \
+	do                   \
+	{                    \
+	} while (0)
+#endif
 
-//functions
-int	init_server(int port);
-void	ft_err_exit(char err_msg[], int socket_fd, struct pollfd *fds);
+// types
+typedef struct client_s
+{
+	int fd;
+	int id;
+	char in[1024];
+	char out[1024];
+} client_t;
 
+// functions
+int init_server(int port);
+void ft_err_exit(char err_msg[], int socket_fd, struct pollfd *fds);
 
 #endif
