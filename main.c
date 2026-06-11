36fb9e3c1521db858e9207a64b2e8637f646553f
masterix_ms <git@sintracon.de>
2026-06-11 17:52:14 +0200

    fix: adapt ft_err_exit to meet the alterations made beforehand

--- main.c
+++ main.c
@@ -5,10 +5,10 @@ int main (int argc, char *argv[])
 	int port = 0;
 
 	if (argc != 2)
-		ft_err_exit("Error, check number of arguments\n", 0);
+		ft_err_exit("Error, check number of arguments\n", 0, NULL);
 	port = atoi(argv[1]);
 	if (!port)
-		ft_err_exit("Error, port number incorrect.\n", 0);
+		ft_err_exit("Error, port number incorrect.\n", 0, NULL);
 	if (init_server(port))
 		return (1);
 	return (0);
